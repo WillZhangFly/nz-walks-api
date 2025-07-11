28004287922058b399288383ac3ea5acd0e6cbf9
Will Zhang <oliverottawa@gmail.com>
2025-07-11 16:05:18 -0400

    feat: setting up identity DbContext

---
+++ Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.Data;
+
+public class NZWalksAuthDbContext : IdentityDbContext
+{
+    public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options)
+        : base(options)
+    {
+
+    }
+}
+
