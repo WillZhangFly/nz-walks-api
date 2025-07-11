bc1bb8363ca0d0c3635241daa72743cbf8b20531
Will Zhang <oliverottawa@gmail.com>
2025-07-11 16:49:10 -0400

    fix: seeding roles

--- Data/NZWalksAuthDbContext.cs
+++ Data/NZWalksAuthDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +9,31 @@ public class NZWalksAuthDbContext : IdentityDbContext
     public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options)
         : base(options)
     {
+    }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var readerId = Guid.Parse("cfa06ed2-bf65-4b65-93ed-c9d286ddb0de");
+        var writerId = Guid.Parse("6884f7d7-ad1f-4101-8df3-7a6fa7387d81");
+
+        var roles = new List<IdentityRole>
+        {
+            new IdentityRole {
+                Id = readerId.ToString(),
+                ConcurrencyStamp = readerId.ToString(),
+                Name = "Reader",
+                NormalizedName = "READER
+         },
+            new IdentityRole {
+                Id = writerId.ToString(),
+                ConcurrencyStamp = writerId.ToString(),
+                Name = "Writer",
+                NormalizedName = "WRITER" },
+        };
+
+        builder.Entity<IdentityRole>().HasData(roles);
     }
 }
 
